6f45bdb576979c3fa3ae4cb24cce9a14f5dae883
Alexandre Herlaud <aherlaud@f6r4s5.clusters.42paris.fr>
2025-08-28 19:06:58 +0200

    new feature (rendering) : branch test and start implementation of the rendering algorithm for one ray

--- src/utils.c
+++ src/utils.c
@@ -6,11 +6,17 @@
 /*   By: aherlaud <aherlaud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/26 16:11:57 by ldevigne          #+#    #+#             */
-/*   Updated: 2025/08/27 12:48:34 by aherlaud         ###   ########.fr       */
+/*   Updated: 2025/08/28 15:44:01 by aherlaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3d.h"
+
+void	ft_error_msg(char *msg, int exit_code)
+{
+	printf("%s", msg);
+	exit(exit_code);
+}
 
 size_t	ft_strlen(const char *str)
 {
