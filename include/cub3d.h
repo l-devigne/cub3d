5e430c2f3bac4bad88b06706ba4e10aa64079014
ldevigne <lucas.devigne@gmail.com>
2025-08-29 11:49:54 +0200

    checking + parsing the struct t_map

--- include/cub3d.h
+++ include/cub3d.h
@@ -27,13 +27,16 @@ typedef struct s_img
 
 typedef struct s_map
 {
-	char **grid;
-	char *north_texture;
-	char *south_texture;
-	char *west_texture;
-	char *east_texture;
-	int floor_color;
-	int ceiling_color;
+	char	*map_path;
+	char	**grid;
+	char	*north_texture;
+	char	*south_texture;
+	char	*west_texture;
+	char	*east_texture;
+	int 	floor_color;
+	int 	ceiling_color;
+	int		x_len;
+	int		y_len;
 } t_map;
 
 typedef struct s_player
@@ -46,13 +49,7 @@ typedef struct s_player
     double plane_y;
 } t_player;
 
-typedef	struct s_tab
-{
-	char	**tab;
-	int		x_len;
-	int		y_len;
-	char	*map_file;
-} t_tab;
+
 
 typedef struct s_data
 {
@@ -70,25 +67,37 @@ typedef struct s_data
 } 				t_data;
 
 // ### QUIT_CLEAN ###
-void ft_clear_all(t_data *data);
-int click_cross(t_data *data);
-int esc_key(int keysym, t_data *data);
+void	ft_clear_all(t_data *data);
+int		click_cross(t_data *data);
+int		esc_key(int keysym, t_data *data);
+void	ft_clear_map(t_map *map);
+void	tab_free_null(char **tab);
+void	char_free_null(char *s);
 
 // ### INIT ###
 t_img initialize_image(void *mlx, int width, int height);
 
-bool	is_map_square(const char *pathname);
-bool	only_valid_chars(const char *pathname);
-bool	is_valid(const char *map);
+
+
+// ### CHECKING ###
+bool	extension_is_correct(const char *map_path, size_t len_map);
+int		get_safe_fd(const char *map_path);
+bool	check_content(const char *map_path);
+bool	is_valid(const char *map_path);
+int		get_x_len(const char *map_path);
+int		get_y_len(const char *map_path);
+
+bool	process_line_check(const char *line);
+bool	is_line_full_wall(char *line);
+bool	map_is_closed_by_walls(const char *pathname);
 
 // ### PARSING ###
+void	fill_map_struct(const char *map_path, t_map *parsed_struct);
+void	display_map(t_map *map);
+
+// ### HANDLE ERROR ###
 void	ft_error_msg(char *msg, int exit_code);
-bool	is_map_square(const char *pathname);
-bool	map_is_closed_by_walls(const char *pathname);
-bool	only_valid_chars(const char *pathname);
-bool	is_valid(const char *map);
-char	**convert_file_to_tab(const char *map);
-int		get_x_len(const char *map);
-int		get_y_len(const char *map);
+
+
 
 #endif
