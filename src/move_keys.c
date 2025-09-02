cf8072f4b1a8e4500cf8d3ee1dd3837976548e23
Meruem <alex.herlaud99@gmail.com>
2025-09-02 23:08:32 +0200

    fix (main : movement) : bring back multiple movement and delete fake wall

--- src/move_keys.c
+++ src/move_keys.c
@@ -6,7 +6,7 @@
 /*   By: meruem <meruem@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/30 14:11:38 by meruem            #+#    #+#             */
-/*   Updated: 2025/09/02 20:24:35 by meruem           ###   ########.fr       */
+/*   Updated: 2025/09/02 22:49:18 by meruem           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ void	move_forward(t_data *data)
 	float	temp_new_x;
 	float	temp_new_y;
 
-	temp_new_x = data->player->x + data->player->dir_x * 0.50;
-	temp_new_y = data->player->y + data->player->dir_y * 0.50;
+	temp_new_x = data->player->x + data->player->dir_x * 0.15;
+	temp_new_y = data->player->y + data->player->dir_y * 0.15;
 	if (!move_to_wall(data, temp_new_x, temp_new_y))
 	{
 		printf("new x is %f and new y is %f\n", data->player->x, data->player->y);
@@ -32,8 +32,8 @@ void	move_backward(t_data *data)
 	float	temp_new_x;
 	float	temp_new_y;
 
-	temp_new_x = data->player->x - data->player->dir_x * 0.50;
-	temp_new_y = data->player->y - data->player->dir_y * 0.50;
+	temp_new_x = data->player->x - data->player->dir_x * 0.15;
+	temp_new_y = data->player->y - data->player->dir_y * 0.15;
 	if (!move_to_wall(data, temp_new_x, temp_new_y))
 	{
 		printf("new x is %f and new y is %f\n", data->player->x, data->player->y);
@@ -47,8 +47,8 @@ void	move_left(t_data *data)
 	float	temp_new_x;
 	float	temp_new_y;
 
-	temp_new_x = data->player->x - (-data->player->dir_y) * 0.50;
-	temp_new_y = data->player->y - data->player->dir_x * 0.50;
+	temp_new_x = data->player->x - (-data->player->dir_y) * 0.15;
+	temp_new_y = data->player->y - data->player->dir_x * 0.15;
 	if (!move_to_wall(data, temp_new_x, temp_new_y))
 	{
 		printf("new x is %f and new y is %f\n", data->player->x, data->player->y);
@@ -62,8 +62,8 @@ void	move_right(t_data *data)
 	float	temp_new_x;
 	float	temp_new_y;
 
-	temp_new_x = data->player->x + (-data->player->dir_y) * 0.50;
-	temp_new_y = data->player->y + data->player->dir_x * 0.50;
+	temp_new_x = data->player->x + (-data->player->dir_y) * 0.15;
+	temp_new_y = data->player->y + data->player->dir_x * 0.15;
 	if (!move_to_wall(data, temp_new_x, temp_new_y))
 	{
 		printf("new x is %f and new y is %f\n", data->player->x, data->player->y);
