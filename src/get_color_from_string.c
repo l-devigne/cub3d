0b2068bae6094c8ed84dcfb9edc5bc05de3a5415
ldevigne <lucas.devigne@gmail.com>
2025-09-07 20:08:23 +0200

    handling more minors edge cases (colors limits mostly)

--- src/get_color_from_string.c
+++ src/get_color_from_string.c
@@ -6,19 +6,65 @@
 /*   By: ldevigne <ldevigne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/02 22:08:15 by ldevigne          #+#    #+#             */
-/*   Updated: 2025/09/03 14:39:14 by ldevigne         ###   ########.fr       */
+/*   Updated: 2025/09/07 19:36:48 by ldevigne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3d.h"
 
-int	set_color_limit(int val)
+bool	rgb_is_ok(char *color, int i)
 {
-	if (val <= 0)
-		val = 0;
-	if (val >= 255)
-		val = 255;
-	return (val);
+	int	count;
+
+	count = 0;
+	if (color[i] == '-')
+		return (false);
+	while (color[i] && color[i] != ',')
+	{
+		if (color[i] >= '0' && color[i] <= '9')
+			count++;
+		i++;
+	}
+	if (count > 3)
+		return (false);
+	return (true);
+}
+
+bool	str_countains_valid_nb_coma(char *str)
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	if (str[0] == ',')
+		return (false);
+	while (str[i])
+	{
+		if (!rgb_is_ok(str, i))
+			return (false);
+		if (str[i] == ',')
+		{
+			count++;
+			if (!rgb_is_ok(str, i + 1))
+				return (false);
+			if (!(str[i + 1] >= '0' && str[i + 1] <= '9'))
+				return (false);
+		}
+		i++;
+	}
+	if (count != 2)
+		return (false);
+	return (true);
+}
+
+bool	check_rgb(int r, int g, int b)
+{
+	if (r > 255 || g > 255 || b > 255)
+		return (false);
+	if (r < 0 || g < 0 || b < 0)
+		return (false);
+	return (true);
 }
 
 int	get_color_from_string(char *str)
@@ -28,6 +74,8 @@ int	get_color_from_string(char *str)
 	int		b;
 	char	*ptr;
 
+	if (!str_countains_valid_nb_coma(str))
+		return (-1);
 	r = ft_atoi(str);
 	ptr = str;
 	while (*ptr && *ptr != ',')
@@ -40,8 +88,7 @@ int	get_color_from_string(char *str)
 	if (*ptr == ',')
 		ptr++;
 	b = ft_atoi(ptr);
-	r = set_color_limit(r);
-	g = set_color_limit(g);
-	b = set_color_limit(b);
+	if (!check_rgb(r, g, b))
+		return (-1);
 	return ((r << 16) | (g << 8) | b);
 }
--- src/get_lines_num_to_skip.c
+++ src/get_lines_num_to_skip.c
@@ -6,7 +6,7 @@
 /*   By: ldevigne <ldevigne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/02 22:06:41 by ldevigne          #+#    #+#             */
-/*   Updated: 2025/09/02 22:06:57 by ldevigne         ###   ########.fr       */
+/*   Updated: 2025/09/07 19:48:11 by ldevigne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,21 +23,32 @@ void	skip_to_end(char **line, int fd)
 		return ;
 }
 
+bool	test_cases(char *line, int i)
+{
+	if (!ft_strncmp(line + i, "NO", 2) || !ft_strncmp(line + i, "SO", 2)
+		|| !ft_strncmp(line + i, "WE", 2) || !ft_strncmp(line + i, "EA", 2)
+		|| !ft_strcmp(line, "\n") || !ft_strncmp(line + i, "F ", 2)
+		|| !ft_strncmp(line + i, "C ", 2))
+		return (true);
+	return (false);
+}
+
 int	get_lines_num_to_skip(const char *map_path)
 {
 	int		fd;
 	char	*line;
 	int		line_count;
+	int		i;
 
 	line_count = 0;
 	fd = get_safe_fd(map_path, KEEP_OPEN);
 	line = get_next_line(fd);
 	while (line)
 	{
-		if (!ft_strncmp(line, "NO", 2) || !ft_strncmp(line, "SO", 2)
-			|| !ft_strncmp(line, "WE", 2) || !ft_strncmp(line, "EA", 2)
-			|| !ft_strcmp(line, "\n") || !ft_strncmp(line, "F ", 2)
-			|| !ft_strncmp(line, "C ", 2))
+		i = 0;
+		while (line[i] == ' ')
+			i++;
+		if (test_cases(line, i))
 		{
 			free(line);
 			line = get_next_line(fd);
